bbb8772844c1bc830ef74411c5d92b67050e6813
Nathan Greene <github@mail.robinhart.me>
2024-05-14 08:39:55 -0700

    remove

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import homeReducer from 'pages/Home/homeSlice';
-
-export default configureStore({
-  reducer: {
-    home: homeReducer,
-  },
-  devTools: process.env.NODE_ENV !== 'production',
-});
--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export default {
-  paths: {
-    home: '/',
-  },
-  links: {
-    readme: 'https://github.com/sachie/react-quickstart#react-quickstart-by-sachie',
-  },
-};
